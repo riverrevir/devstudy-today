8032c097151d9713cb506cf0edcc2ca6afe18043
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-06-05 20:06:18 +0900

    refactor : securityconfig(403 권한 에러 수정),usercontroller 코드 수정

--- src/main/java/today/devstudy/config/SecurityConfig.java
+++ src/main/java/today/devstudy/config/SecurityConfig.java
@@ -13,8 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests().antMatchers("/**").permitAll()
-                ;
+        http.cors().and().csrf().disable();
     }
 
     @Bean
--- src/main/java/today/devstudy/user/UserController.java
+++ src/main/java/today/devstudy/user/UserController.java
@@ -22,7 +22,7 @@ public class UserController {
     List<String> gender;
 
     @ModelAttribute
-    public void genderLoad(){
+    public void genderLoad() {
         gender = new ArrayList<String>();
         gender.add("남");
         gender.add("여");
@@ -30,34 +30,35 @@ public class UserController {
 
     /**
      * register(회원가입)
+     *
      * @param model
      * @param userCreateForm
      * @return
      */
     @GetMapping("/register")
-    public String register(Model model, UserCreateForm userCreateForm){
-        model.addAttribute("sex",gender);
+    public String register(Model model, UserCreateForm userCreateForm) {
+        model.addAttribute("sex", gender);
         return "register_form";
     }
 
     @PostMapping("/register")
-    public String register(@Valid UserCreateForm userCreateForm,BindingResult bindingResult){
-        if(bindingResult.hasErrors()){
+    public String register(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
             return "register_form";
         }
-        if(!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())){
-            bindingResult.rejectValue("password2","passwordInCorrect","패스워드가 일치하지 않습니다.");
+        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+            bindingResult.rejectValue("password2", "passwordInCorrect", "패스워드가 일치하지 않습니다.");
             return "register_form";
         }
         try {
             userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1(), userCreateForm.getSex());
-        }catch(DataIntegrityViolationException e){
+        } catch (DataIntegrityViolationException e) {
             e.printStackTrace();
-            bindingResult.reject("registerFailed","이미 등록된 사용자입니다.");
+            bindingResult.reject("registerFailed", "이미 등록된 사용자입니다.");
             return "register_form";
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-            bindingResult.reject("registerFailed",e.getMessage());
+            bindingResult.reject("registerFailed", e.getMessage());
             return "register_form";
         }
         return "redirect:/";
@@ -65,16 +66,16 @@ public class UserController {
 
 
     @GetMapping("/login")
-    public String login(){
+    public String login() {
 
         return "login_form";
     }
 
     @PostMapping("/login")
-    public String login(String username,String password){
-        try{
-            userService.login(username,password);
-        } catch(Exception e){
+    public String login(String username, String password) {
+        try {
+            userService.login(username, password);
+        } catch (Exception e) {
             System.out.println("3번오류");
             return "login_form";
         }
