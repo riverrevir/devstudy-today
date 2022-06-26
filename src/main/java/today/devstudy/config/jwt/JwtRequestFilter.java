d0d2c7de1fe443b563eb385216939cfc1eea5dde
DESKTOP-G3USBLG\simba <simba0502@naver.com>
2022-06-26 21:50:48 +0900

    [fix] User패키지 변경

--- src/main/java/today/devstudy/config/jwt/JwtRequestFilter.java
+++ src/main/java/today/devstudy/config/jwt/JwtRequestFilter.java
@@ -2,24 +2,19 @@ package today.devstudy.config.jwt;
 
 import groovy.util.logging.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import today.devstudy.domain.User;
-import today.devstudy.repository.UserRepository;
-import today.devstudy.service.JwtUserDetailService;
+import today.devstudy.domain.user.User;
+import today.devstudy.domain.user.UserRepository;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.security.SignatureException;
 
 @Slf4j
 @Component
