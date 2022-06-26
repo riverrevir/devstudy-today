a9e3750860e431e6c1db5a5b82815c096aaa4f2f
DESKTOP-G3USBLG\simba <simba0502@naver.com>
2022-06-26 21:53:57 +0900

    [fix] 불필요 메서드 삭제 및 패키지 변경으로 인한 import 수정

--- src/test/java/today/devstudy/service/RegisterTest.java
+++ src/test/java/today/devstudy/service/RegisterTest.java
@@ -1,13 +1,11 @@
 package today.devstudy.service;
 
 
-import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-import today.devstudy.domain.User;
 
 @SpringBootTest
 @Transactional
@@ -16,8 +14,4 @@ public class RegisterTest {
     @Autowired
     private UserService userService;
 
-    @Test
-    public void register() {
-        User user = userService.create("id", "email@co.kr", "1234", "male");
-    }
 }
