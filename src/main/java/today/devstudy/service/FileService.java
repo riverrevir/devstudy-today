62cdaf57b233b8211d423bc384662ae303afee4e
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-08-19 11:49:03 +0900

    fixed: 확장자 . 문제 해결

--- src/main/java/today/devstudy/service/FileService.java
+++ src/main/java/today/devstudy/service/FileService.java
@@ -14,12 +14,13 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class FileService {
-    private static final String root="/home/devstudy/img/";
-    private static final String baseProfile="base-profile";
+    private static final String root = "/home/devstudy/img/";
+    private static final String baseProfile = "base-profile";
     private final UserRepository userRepository;
-    public void downloadImg(String userId, HttpServletResponse response){
-        User user=userRepository.findByUserId(userId).orElseThrow(()->new IllegalArgumentException("해당 유저는 존재하지 않습니다."));
-        String imgName=user.getProfile();
+
+    public void downloadImg(String userId, HttpServletResponse response) {
+        User user = userRepository.findByUserId(userId).orElseThrow(() -> new IllegalArgumentException("해당 유저는 존재하지 않습니다."));
+        String imgName = user.getProfile();
         String imgSrc = new StringBuilder(root).append("/").append(imgName).append(".").append(user.getType()).toString();
         try (FileInputStream fis = new FileInputStream(imgSrc);
              OutputStream out = response.getOutputStream();
@@ -30,33 +31,32 @@ public class FileService {
                 out.write(buffer, 0, readCount);
             }
         } catch (FileNotFoundException e) {
-            throw new IllegalArgumentException("",e);
+            throw new IllegalArgumentException("", e);
         } catch (IOException e) {
             throw new IllegalArgumentException(e);
         }
     }
 
     public void uploadImg(String userId, MultipartFile file, HttpServletResponse response) throws IOException {
-        User user=userRepository.findByUserId(userId).orElseThrow(()->new IllegalArgumentException("해당 유저는 존재하지 않습니다."));
-        String contentType=file.getContentType();
+        User user = userRepository.findByUserId(userId).orElseThrow(() -> new IllegalArgumentException("해당 유저는 존재하지 않습니다."));
+        String contentType = file.getContentType();
         String fileExtension;
-        deleteImg(user.getProfile(),user.getType());
-        if(ObjectUtils.isEmpty(contentType)){ //기본 프로필로 변경요청
+        deleteImg(user.getProfile(), user.getType());
+        if (ObjectUtils.isEmpty(contentType)) { //기본 프로필로 변경요청
             user.setProfile(baseProfile);
-            user.setType(".jpg");
-        }
-        else{ // 새로운 프로필로 변경요청
-            if(contentType.contains("image/jpeg")){
-                fileExtension=".jpg";
-            } else if(contentType.contains("image/png")){
-                fileExtension=".png";
-            } else{
-                response.sendError(404,"올바르지 않은 파일입니다.");
+            user.setType("jpg");
+        } else { // 새로운 프로필로 변경요청
+            if (contentType.contains("image/jpeg")) {
+                fileExtension = "jpg";
+            } else if (contentType.contains("image/png")) {
+                fileExtension = "png";
+            } else {
+                response.sendError(404, "올바르지 않은 파일입니다.");
                 return;
             }
-            UUID uuid=UUID.randomUUID();
-            String uploadFileName=uuid.toString()+"_"+userId;
-            File saveFile=new File(root+File.separator+uploadFileName+fileExtension);
+            UUID uuid = UUID.randomUUID();
+            String uploadFileName = uuid.toString() + "_" + userId;
+            File saveFile = new File(root + File.separator + uploadFileName + "." + fileExtension);
             file.transferTo(saveFile);
             user.setProfile(uploadFileName);
             user.setType(fileExtension);
@@ -64,9 +64,10 @@ public class FileService {
         userRepository.save(user);
         response.setStatus(HttpServletResponse.SC_OK);
     }
-    public void deleteImg(String userProfile,String fileExtension){
-        if(!userProfile.equals(baseProfile)) {
-            File deleteFile = new File(root + File.separator + userProfile + fileExtension);
+
+    public void deleteImg(String userProfile, String fileExtension) {
+        if (!userProfile.equals(baseProfile)) {
+            File deleteFile = new File(root + File.separator + userProfile + "." + fileExtension);
             if (deleteFile.exists()) {
                 deleteFile.delete();
             }
